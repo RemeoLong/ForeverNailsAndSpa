79f080d9e3f02fce80629d4158adcb9ed4150b20
RemeoLong <remeolong82@yahoo.com>
2020-08-26 20:28:09 -0500

    change settings os

--- DjangoProject/mysite/wsgi.py
+++ DjangoProject/mysite/wsgi.py
@@ -6,11 +6,22 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
+import os
+import sys
+
+# add your project directory to the sys.path
+project_home = '/home/RemeoLong/mysite'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_applic
