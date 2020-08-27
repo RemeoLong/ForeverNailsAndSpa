d6994f35c13165e9b63aff0688cf9fadbac0e3b3
RemeoLong <remeolong82@yahoo.com>
2020-08-26 21:21:41 -0500

    change html

--- DjangoProject/mysite/wsgi.py
+++ DjangoProject/mysite/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 import os
 import sys
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 # add your project directory to the sys.path
 project_home = '/home/RemeoLong/mysite'
@@ -18,10 +21,5 @@ if project_home not in sys.path:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
-# serve django via WSGI
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-import os
 
-from django.core.wsgi import get_wsgi_applic
