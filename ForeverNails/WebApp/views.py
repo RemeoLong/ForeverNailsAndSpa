bf28aa54baff91d11b16d9adc55a7baa4e707663
RemeoLong <remeolongquangtran@gmail.com>
2020-09-13 21:36:36 -0500

    html changes

--- ForeverNails/WebApp/views.py
+++ ForeverNails/WebApp/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .covid_form import PostForm
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+def Main(request):
+    return render(request, 'blog/Main.html', {})
+
+
+def Menu(request):
+    return render(request, 'blog/Menu.html', {})
+
+def boot(request):
+    return render(request, 'blog/boot.html', {})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
 
