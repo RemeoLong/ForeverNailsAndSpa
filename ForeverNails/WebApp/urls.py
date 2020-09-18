d3a4edf13def509413edb725bfc34db0030664c9
RemeoLong <remeolongquangtran@gmail.com>
2020-09-17 19:23:08 -0500

    LogIn

--- ForeverNails/WebApp/urls.py
+++ ForeverNails/WebApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.Main, name='Main'),
     path('ServiceMenu', views.Menu, name='Menu'),
     path('boot', views.boot, name='boot'),
-    path('Log In', views.LogIN, name='LogIN'),
+    path('Log_In', views.LogIN, name='LogIN'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
 ]
