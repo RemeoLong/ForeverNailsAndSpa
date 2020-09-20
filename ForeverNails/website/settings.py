ffe95189fe08548776d0c7a80df5a6a4790ce03a
RemeoLong <remeolongquangtran@gmail.com>
2020-09-20 14:35:33 -0500

    mistake

--- ForeverNails/website/settings.py
+++ ForeverNails/website/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'WebApp.apps.WebappConfig',
     'django.contrib.admin',
-    'schedule',
     'djangobower',
 ]
 
--- ForeverNails/website/wsgi.py
+++ ForeverNails/website/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import os
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
