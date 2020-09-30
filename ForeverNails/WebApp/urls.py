ca4ee61133ca5b170baca2ad734a736bc314c745
RemeoLong <remeolongquangtran@gmail.com>
2020-09-30 15:12:57 -0500

    Login Page

--- ForeverNails/WebApp/urls.py
+++ ForeverNails/WebApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.Main, name='Main'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('Contact', views.Contact, name='Contact'),
     path('location', views.location, name='location'),
     path('login', views.LogIN, name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
 ]
